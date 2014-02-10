0243bdba509f91723374e5b31ea939355b554881
Rui Zhang <rui.zhang@mongodb.com>
2014-02-10 14:58:48 -0800

    add dbpath, and dbpath mkdir for mongo install.sh

--- config/mongo_config/install.sh
+++ config/mongo_config/install.sh
@@ -27,3 +27,8 @@ tar zxvf mongodb-linux-x86_64-latest.tgz
 
 echo "create mongod.conf"
 echo "# auto generated mongod config " > /home/vagrant/mongod.conf
+echo "dbpath=/home/vagrant/dbs" >> /home/vagrant/mongod.conf
+
+echo "create database folder"
+mkdir /home/vagrant/dbs
+
