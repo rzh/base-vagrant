a620a9aba318aa3ed116c30ec92caeff8b3065d7
Rui Zhang <rui.zhang@mongodb.com>
2014-02-12 14:46:50 -0800

    add rhel58n for gssapi

--- gssapi/config/config_dns.sh
+++ gssapi/config/config_dns.sh
@@ -3,5 +3,6 @@
 echo "Configuring /etc/hosts ..."
 echo "192.168.19.100	kdc.mongotest.com kdc"   >> /etc/hosts
 echo "192.168.19.101	rhel64.mongotest.com rhel64"   >> /etc/hosts
+echo "192.168.19.102	rhel58.mongotest.com rhel58"   >> /etc/hosts
 echo "192.168.19.150	client.mongotest.com client"   >> /etc/hosts
 
